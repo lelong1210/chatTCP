de5190daf48a2a701b6702b23b29b37716fc1ac7
Your Name <you@example.com>
2022-09-18 10:08:43 +0700

    start

---
+++ demo/src/gui/ClientTransferMain.java
@@ -0,0 +1,8 @@
+package gui;
+
+public class ClientTransferMain {
+    public static void main(String[] args) {
+        ClientTransferView view = new ClientTransferView();
+        new ClientTransferController(view);
+    }
+}
